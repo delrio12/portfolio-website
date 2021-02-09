a7c62df11cb8d3e3fd0bc3d6935f5203106ff684
Andres Del Rio <addelrio11@gmail.com>
2021-02-09 00:01:36 -0500

    f

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -17,7 +17,7 @@ class ContactForm extends React.Component {
     e.preventDefault();
     axios({
       method: "POST", 
-      url:"https://us-central1-portfolio-website-582f4.cloudfunctions.net/submit", 
+      url:"http://localhost:3002/send", 
       data:  this.state
     }).then((response)=>{
       if (response.data.status === 'success') {
