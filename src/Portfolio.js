12feade6054bfa62c73afc7f4ddf74529c5e377d
Andres Del Rio <addelrio11@gmail.com>
2021-03-08 14:09:17 -0500

    twitter-clone project added

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -78,6 +78,69 @@ function Portfolio() {
                         </div>
                     </div>    
                 </div>
+
+
+
+                <div className="portfolio__project">
+                    <img className="portfolio__project__image" src="https://firebasestorage.googleapis.com/v0/b/portfolio-website-582f4.appspot.com/o/images%2Ftwitter-clone.png?alt=media&token=7252c97e-90de-4c3f-9f18-e5f2c0352fd1" alt="twitter-clone" />
+                    <div className="portfolio__project__image__overlay">
+                        <div className="portfolio__project__description">
+                            <h3>Twitter Clone</h3>
+                            <p>Built a clone of the Twitter homepage.</p>
+                            <ul>
+                                <li className="portfolio__button">
+                                    <a  href="#" target="_blank" href="https://twitter-clone-55b33.web.app/">
+                                        <button type="button" className="btn btn-light btn-sm">Go to Website</button>
+                                    </a>
+                                </li>
+                                <li className="portfolio__button">
+                                    <a  href="#" target="_blank" href="https://github.com/delrio12/twitter-clone">
+                                        <button type="button" className="btn btn-warning btn-sm">Go to Github Rep</button>
+                                    </a>
+                                </li>  
+                            </ul>
+                            <p>Technologies I used for this project:</p>
+                            <ul className="three-col">
+                                <li className="portfolio__button">
+                                    <button className="btn btn-secondary btn-sm" disabled>
+                                        HTML5
+                                    </button>
+                                </li>
+                                <li className="portfolio__button">
+                                    <button className="btn btn-secondary btn-sm"  disabled>
+                                        CSS
+                                    </button>
+                                </li>
+                                <li className="portfolio__button">
+                                    <button className="btn btn-secondary btn-sm"  disabled>
+                                        JAVASCRIPT
+                                    </button>
+                                </li>
+                                <li className="portfolio__button">
+                                    <button className="btn btn-secondary btn-sm"  disabled>
+                                        REACTJS
+                                    </button>
+                                </li>
+                                <li className="portfolio__button">
+                                    <button className="btn btn-secondary btn-sm"  disabled>
+                                        FLEXBOX
+                                    </button>
+                                </li>
+                                <li className="portfolio__button">
+                                    <button className="btn btn-secondary btn-sm"  disabled>
+                                        FIREBASE
+                                    </button>
+                                </li>
+                                <li className="portfolio__button">
+                                    <button className="btn btn-secondary btn-sm"  disabled>
+                                        MATERIAL-UI
+                                    </button>
+                                </li>
+                            </ul>
+                            
+                        </div>
+                    </div>    
+                </div>
                 {/* <div className="portfolio__project">
                     <img className="portfolio__project__image" src="https://digitalsynopsis.com/wp-content/uploads/2017/09/free-stock-images-photos-websites-pexels.jpg" alt="Forest" />
                     <div className="portfolio__project__image__overlay">
